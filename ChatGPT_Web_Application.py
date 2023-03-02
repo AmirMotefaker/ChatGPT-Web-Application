ea46406f64d7cb4da7ab707a046a4dfd5a96fe5e
Amir Motefaker <amotef@gmail.com>
2023-03-02 18:08:34 +0330

    Update ChatGPT_Web_Application.py

--- ChatGPT_Web_Application.py
+++ ChatGPT_Web_Application.py
@@ -1,18 +1,49 @@
-import streamlit as st
+# import streamlit as st
 
-st.title("ChatGPT-like Web App")
-#storing the chat
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-user_input=st.text_input("You:",key='input')
-if user_input:
-    output=generate_response(user_input)
-    #store the output
-    st.session_state['past'].append(user_input)
-    st.session_state['generated'].append(output)
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+# st.title("ChatGPT-like Web App")
+# #storing the chat
+# if 'generated' not in st.session_state:
+#     st.session_state['generated'] = []
+# if 'past' not in st.session_state:
+#     st.session_state['past'] = []
+# user_input=st.text_input("You:",key='input')
+# if user_input:
+#     output=generate_response(user_input)
+#     #store the output
+#     st.session_state['past'].append(user_input)
+#     st.session_state['generated'].append(output)
+# if st.session_state['generated']:
+#     for i in range(len(st.session_state['generated'])-1, -1, -1):
+#         message(st.session_state["generated"][i], key=str(i))
+#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
+
+# this loop will let us ask questions continuously
+
+while True:
+    
+    # Set up the model and prompt
+    model_engine = "text-davinci-003"
+    
+    prompt = input('Enter new prompt: ')
+
+    if 'exit' in prompt or 'quit' in prompt:
+        break
+
+    # Generate a response
+    # given the most recent context (4096 characters)
+    # continue the text up to 2048 tokens ~ 8192 charaters
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    
+    # extracting useful part of response
+    response = completion.choices[0].text
+    
+    # printing response
+    print(response)
