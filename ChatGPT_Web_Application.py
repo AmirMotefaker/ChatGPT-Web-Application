a7d0a4c8f261e6259ebcece65c7910ba6a628e6e
Amir Motefaker <amotef@gmail.com>
2023-03-02 18:01:04 +0330

    Create ChatGPT_Web_Application.py

---
+++ ChatGPT_Web_Application.py
@@ -0,0 +1,18 @@
+import streamlit as 
+
+st.title("ChatGPT-like Web App")
+#storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("You:",key='input')
+if user_input:
+    output=generate_response(user_input)
+    #store the output
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
